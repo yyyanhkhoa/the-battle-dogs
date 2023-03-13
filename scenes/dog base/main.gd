44f50b9547b3515c05e18e8126b40567ddda5e56
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2023-03-13 10:34:17 +0700

    up dog base

--- scenes/dog base/main.gd
+++ scenes/dog base/main.gd
@@ -10,7 +10,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_nut_bat_dau_pressed():
-	get_tree().change_scene_to_file("res://scenes/dog base/main.tscn")
